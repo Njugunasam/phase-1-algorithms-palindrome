ca6111a8779e02778988ca7ccafa2246f98beb10
Njugunasam <sammynjuguna5826@gmail.com>
2023-10-25 19:26:46 +0300

    done

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const cleanWord = word.replace(/[^a-z]/g, '').toLowerCase();
+  return cleanWord === cleanWord.split('').reverse().join('');
 }
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
